51ecafc9c9d9d0c4e94682476599b815abbdd4d2
Steven Scangas <scangas.steven@gmail.com>
2024-04-09 19:09:27 -0400

    added badges for players that have completed the escaperoom

--- frontend/src/components/SocialSidebar/PlayerName.tsx
+++ frontend/src/components/SocialSidebar/PlayerName.tsx
@@ -5,15 +5,23 @@ type PlayerNameProps = {
   player: PlayerController;
 };
 
+import Image from 'next/image';
+
 const DisplayUsername = (player: PlayerController) => {
-  {
-    if (player.completed) {
-      return player.userName + '(Completed Escape Room!)';
-    } else {
-      return player.userName;
-    }
+  if (player.completed) {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        {player.userName}
+        <div style={{ width: '25px', height: '25px' }}>
+          <Image src='/assets/escaperoombadge.png' width={25} height={25} alt='Escape Room Badge' />
+        </div>
+      </div>
+    );
+  } else {
+    return player.userName;
   }
 };
+
 export default function PlayerName({ player }: PlayerNameProps): JSX.Element {
   return <>{DisplayUsername(player)}</>;
 }
